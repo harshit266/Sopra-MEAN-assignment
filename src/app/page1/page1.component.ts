7b4e91837f3abfda60ac2d877bf55cf88591a14e
harshit <harshit.vayuz@gmail.com>
2020-10-11 16:16:19 +0530

    email and number validation

--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -16,6 +16,25 @@ export class Page1Component implements OnInit {
   ngOnInit() {
   }
   saveValue(f){
+
+//email validation
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@soprabanking.com/.test(f.email))
+  {}
+  else{  
+  alert("You have entered an invalid email address! or entered address is not of @soprabanking.com")
+    return (false)
+  }
+
+//number validation
+  var phoneno = /^\d{10}$/;
+  if (/^\d{10}$/.test(f.number)){}
+    
+      else
+        {
+        alert("Entered Number should be 10 digits");
+        return false;
+        }
+
     const obj = {
       firstName: f.firstName,
       lastName:f.lastName,
